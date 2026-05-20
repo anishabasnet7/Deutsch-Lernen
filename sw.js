0f2fc67795171976aa1f640da0941e7ec2b92a2e
anishabasnet7 <anishabasnet50@gmail.com>
2026-05-20 21:50:13 +0200

    mobile offline

--- sw.js
+++ sw.js
@@ -1,16 +1,48 @@
-const CACHE_NAME = "german-app-v1";
+const CACHE_NAME = "german-app-v2"; // Incremented version to force update
 
 const urlsToCache = [
   "/",
   "/index.html",
-  "/article.html"
+  "/article.html",
+  "/gender.html",
+  "/preposition.html",
+  "/verb.html",
+  "/tense.html",
+  "/pronoun.html",
+  "/wordorder.html",
+  "/number.html",
+  "/connector.html",
+  "/pronunciation.html",
+  "/prefix.html",
+  "/compound.html",
+  "/school.html",
+  "/slang.html",
+  "/navbar.html",
+  "/footer.html",
+  "/style.css",
+  "/manifest.json",
+  "/icon.png" // Include your icon if it exists
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
-    })
+    }).then(() => self.skipWaiting()) // Forces the waiting service worker to become active
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // Delete old caches
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Takes control of open pages immediately
   );
 });
 
