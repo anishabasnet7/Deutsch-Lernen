7e646a44eb227f2300d96a98b23b182f14636675
anishabasnet7 <anishabasnet50@gmail.com>
2026-05-20 22:06:35 +0200

    mobile offline

--- sw.js
+++ sw.js
@@ -1,27 +1,27 @@
-const CACHE_NAME = "german-app-v2"; // Incremented version to force update
+const CACHE_NAME = "german-app-v3"; // Incremented to force update
 
 const urlsToCache = [
   "./",
-  "/index.html",
-  "/article.html",
-  "/gender.html",
-  "/preposition.html",
-  "/verb.html",
-  "/tense.html",
-  "/pronoun.html",
-  "/wordorder.html",
-  "/number.html",
-  "/connector.html",
-  "/pronunciation.html",
-  "/prefix.html",
-  "/compound.html",
-  "/school.html",
-  "/slang.html",
-  "/navbar.html",
-  "/footer.html",
-  "/style.css",
-  "/manifest.json",
-  "/icon.png" 
+  "index.html",
+  "article.html",
+  "gender.html",
+  "preposition.html",
+  "verb.html",
+  "tense.html",
+  "pronoun.html",
+  "wordorder.html",
+  "number.html",
+  "connector.html",
+  "pronunciation.html",
+  "prefix.html",
+  "compound.html",
+  "school.html",
+  "slang.html",
+  "navbar.html",
+  "footer.html",
+  "style.css",
+  "manifest.json",
+  "icon.png"
 ];
 
 self.addEventListener("install", event => {
